587eb2e4afe30e7b4e5f14317475a16699498f5d
tony <wabslygzj@163.com>
2019-07-30 09:19:26 +0800

    po

---
+++ spring-boot-batch/src/main/java/com/tony/batch/Person.java
@@ -0,0 +1,25 @@
+package com.tony.batch;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @author tony
+ * @describe Person
+ * @date 2019-07-30
+ */
+@Data
+@ToString
+@Accessors(fluent = true, prefix = "test")
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.firstName("testPeter").lastName("testSmith");
+        System.out.println(p.firstName());
+        System.out.println(p.lastName());
+    }
+}
