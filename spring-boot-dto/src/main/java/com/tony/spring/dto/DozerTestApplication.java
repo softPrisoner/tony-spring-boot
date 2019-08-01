10c21b0ab496b4d036349ab427f0465b790056b2
tony <wabslygzj@163.com>
2019-08-01 17:25:49 +0800

    init

---
+++ spring-boot-dto/src/main/java/com/tony/spring/dto/DozerTestApplication.java
@@ -0,0 +1,16 @@
+package com.tony.spring.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tony
+ * @describe DozerTestApplication
+ * @date 2019-08-01
+ */
+@SpringBootApplication
+public class DozerTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DozerTestApplication.class, args);
+    }
+}
