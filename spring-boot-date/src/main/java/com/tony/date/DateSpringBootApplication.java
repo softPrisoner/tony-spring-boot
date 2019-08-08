a54d3beb2ab17c225cc14230755bcc4443347526
Administrator <51616330@qq.com>
2019-08-08 23:50:30 +0800

    commit

---
+++ spring-boot-date/src/main/java/com/tony/date/DateSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.tony.date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tony
+ * @describe DateSpringBootApplication
+ * @date 2019/08/08
+ */
+@SpringBootApplication
+public class DateSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DateSpringBootApplication.class, args);
+    }
+}
