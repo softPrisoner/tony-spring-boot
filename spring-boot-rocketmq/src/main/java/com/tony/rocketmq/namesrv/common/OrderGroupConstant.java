8bced4d1a9769216cdaf2342647a451462ec45f0
tony <wabslygzj@163.com>
2019-08-19 23:06:37 +0800

    order group constant

---
+++ spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/common/OrderGroupConstant.java
@@ -0,0 +1,11 @@
+package com.tony.rocketmq.namesrv.common;
+
+/**
+ * @author tony
+ * @describe OrderGroupConstant
+ * @date 2019-08-19
+ */
+public class OrderGroupConstant {
+    public static final String ORDER_GROUP = "ORDER-GROUP";
+    public static final String NAME_ADDR = "localhost:9876";
+}
