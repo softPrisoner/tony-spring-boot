e7dc7a65119004d1214e51c613124539bf5bc308
tony <wabslygzj@163.com>
2019-08-20 20:27:00 +0800

    sync producer update

--- spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/producer/SyncProducer.java
+++ spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/producer/SyncProducer.java
@@ -5,10 +5,16 @@ import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
 import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.remoting.RPCHook;
 import com.alibaba.rocketmq.remoting.common.RemotingHelper;
 import com.alibaba.rocketmq.remoting.exception.RemotingException;
+import com.alibaba.rocketmq.remoting.protocol.RemotingCommand;
+import com.google.common.base.Preconditions;
 
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author tony
@@ -16,11 +22,27 @@ import java.io.UnsupportedEncodingException;
  * @date 2019-08-19
  */
 public class SyncProducer {
+    private static final Lock lock = new ReentrantLock(false);
+
     public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
         // real time wait for success
-        DefaultMQProducer producer = new DefaultMQProducer("order-group");
+        DefaultMQProducer producer = null;
+        if (lock.tryLock(2, TimeUnit.SECONDS)) {
+            producer = new DefaultMQProducer("order-group", new RPCHook() {
+                @Override
+                public void doBeforeRequest(String s, RemotingCommand remotingCommand) {
+                    System.out.println("Before send message from producer:" + s + "code:" + remotingCommand.getCode());
+                }
+
+                @Override
+                public void doAfterResponse(String s, RemotingCommand remotingCommand, RemotingCommand remotingCommand1) {
+                    System.out.println("After send message from producer:" + s + "code:" + remotingCommand.getCode());
+                }
+            });
+        }
+        lock.unlock();
+        Preconditions.checkNotNull(producer, "illegal state:producer is null");
         producer.setNamesrvAddr("localhost:9876");
-        producer.setCreateTopicKey("");
         producer.start();
         Message message = new Message("orderTopic"
                 , "TagA", "Hello RocketMQ".getBytes(RemotingHelper.DEFAULT_CHARSET));
@@ -30,4 +52,5 @@ public class SyncProducer {
                 + result.getTransactionId() + " "
                 + result.getSendStatus());
     }
+
 }
