d7b3aa87704b1b9d5d0c2c2b93e683ed2a95a508
tony <wabslygzj@163.com>
2019-08-20 23:29:21 +0800

    async consumer test

---
+++ spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/consumer/AsyncConsumer.java
@@ -0,0 +1,38 @@
+package com.tony.rocketmq.namesrv.consumer;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+/**
+ * @author tony
+ * @describe SyncConsumer
+ * @date 2019-08-19
+ */
+public class AsyncConsumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("Group-Async-Test");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("TP-Async-Test", "TagA");
+        consumer.registerMessageListener((MessageListenerConcurrently)
+
+                (messages, consumeConcurrentlyContext) -> {
+
+                    for (MessageExt message : messages) {
+                        //print info
+                        try {
+                            //make up with dealing with business
+                            Thread.sleep(30000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(message.toString());
+                    }
+
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                });
+        consumer.start();
+    }
+}
