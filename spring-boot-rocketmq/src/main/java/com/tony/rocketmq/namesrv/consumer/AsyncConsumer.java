ab85370e1340446e82a75cc842e1f9c4f4de9da5
tony <wabslygzj@163.com>
2019-08-22 08:49:58 +0800

    add consume from where test

--- spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/consumer/AsyncConsumer.java
+++ spring-boot-rocketmq/src/main/java/com/tony/rocketmq/namesrv/consumer/AsyncConsumer.java
@@ -4,6 +4,7 @@ import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
 /**
@@ -16,8 +17,10 @@ public class AsyncConsumer {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("Group-Async-Test");
         consumer.setNamesrvAddr("localhost:9876");
         consumer.subscribe("TP-Async-Test", "TagA");
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_TIMESTAMP);
         consumer.registerMessageListener((MessageListenerConcurrently)
-
                 (messages, consumeConcurrentlyContext) -> {
 
                     for (MessageExt message : messages) {
