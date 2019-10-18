d7afc7d85169a5319f1e8e67779a1d122203acce
tony <wabslygzj@163.com>
2019-10-18 08:59:23 +0800

    update the result class of restful interface with reduce the garbage code.

--- spring-boot-activiti/src/main/java/com/rainbow/tony/activiti/common/ListResult.java
+++ spring-boot-activiti/src/main/java/com/rainbow/tony/activiti/common/ListResult.java
@@ -1,21 +1,75 @@
 package com.rainbow.tony.activiti.common;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
-
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 列表集合返回结果封装类
+ *
  * @author tony
  * @describe ListResult
- * @date 2019-08-31
+ * @date 2019-09-07
  */
-@Data
-@Accessors(chain = true)
-public class ListResult<T> {
-    private String message;
+public class ListResult<T> implements Serializable {
+    private static final long serialVersionUID = 3833672216576011243L;
+
+    private int code;
     private boolean success;
     private List<T> data;
-    private int code;
+    private String message;
+    private static final int SUCCESS = 200;
 
+    public ListResult<T> success(List<T> data, String message) {
+
+        this.code = SUCCESS;
+        this.success = true;
+        this.data = (data == null ? new ArrayList<>() : data);
+        this.message = message;
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public ListResult<T> setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public ListResult<T> setSuccess(boolean success) {
+        this.success = success;
+        return this;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public ListResult<T> setData(List<T> data) {
+        this.data = data;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ListResult<T> setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public ListResult<T> error(int code, String message) {
+        this.success = false;
+        this.code = code;
+        this.message = message;
+        this.data = new ArrayList<>();
+        return this;
+    }
 }
--- spring-boot-activiti/src/main/java/com/rainbow/tony/activiti/common/PlainResult.java
+++ spring-boot-activiti/src/main/java/com/rainbow/tony/activiti/common/PlainResult.java
@@ -1,18 +1,76 @@
 package com.rainbow.tony.activiti.common;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
+import java.io.Serializable;
 
 /**
+ * 普通返回结果封装类
+ *
  * @author tony
  * @describe PlainResult
- * @date 2019-08-31
+ * @date 2019-09-07
  */
-@Data
-@Accessors(chain = true)
-public class PlainResult<T> {
-    private String message;
+public class PlainResult<T> implements Serializable {
+    private static final long serialVersionUID = 357509687400002037L;
+
+    private int code;
     private boolean success;
     private T data;
-    private int code;
+    private String message;
+    private static final int SUCCESS = 200;
+
+    public PlainResult<T> success(T data, String message) {
+        this.code = SUCCESS;
+        this.success = true;
+        this.data = data;
+        this.message = message;
+        return this;
+    }
+
+    public PlainResult<T> build() {
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public PlainResult<T> setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public PlainResult<T> setSuccess(boolean success) {
+        this.success = success;
+        return this;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public PlainResult<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public PlainResult<T> setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public PlainResult<T> error(int code, String message) {
+        this.success = false;
+        this.code = code;
+        this.message = message;
+        this.data = null;
+        return this;
+    }
 }
