193af9757827d5f6224824f3c93acb3099a1bb62
tony <wabslygzj@163.com>
2019-11-24 16:46:46 +0800

    test commit

--- spring-boot-test/src/main/java/com/rainbow/tony/test/design/webfilter/HTMLFilter.java
+++ spring-boot-test/src/main/java/com/rainbow/tony/test/design/webfilter/HTMLFilter.java
@@ -4,11 +4,9 @@ public class HTMLFilter implements Filter {
     @Override
     public void doFilter(Request request, Response response, FilterChain chain) {
         if (1 != 2)
-            chain.pass = false;
-        request.requestStr = request.requestStr.replace("2", "3") + "-----HTMLFilter";
+            request.requestStr = request.requestStr.replace("2", "3") + "-----HTMLFilter";
         chain.doFilter(request, response, chain);
         response.responseStr = response.responseStr.replace("3", "2") + "-----HTMLFilter";
-
     }
 
 }
